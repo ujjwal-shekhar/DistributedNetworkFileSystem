034b515ac14df51923d9e24b10ec77444fccf62a
Ujjwal Shekhar <ujjwal.shekhar@research.iiit.ac.in>
2026-05-08 19:04:57 +0000

    tiny change in build script

--- build_simple.sh
+++ build_simple.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
-/snap/bin/cmake -S . -B build -G Ninja && /snap/bin/cmake --build build
+
+# Explicitly set the compiler to g++-16
+/snap/bin/cmake -S . -B build -G Ninja \
+    -DCMAKE_CXX_COMPILER=g++-16 \
+    -DCMAKE_CXX_SCAN_FOR_MODULES=ON
+
+/snap/bin/cmake --build build
